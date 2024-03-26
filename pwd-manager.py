d8ccc502b8dbb8b035b67cbfc9d0f0bb39f253c0
JhonnyMartz <155391339+JhonnyMartz@users.noreply.github.com>
2024-03-26 19:52:09 -0400

    Update pwd-manager.py

--- pwd-manager.py
+++ pwd-manager.py
@@ -1,1 +1,22 @@
-hello world
+import threading
+import socket
+
+
+try: 
+    cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[c]: SOCKET CREATED")
+except socket.erorr as err:
+    print("socket open error")
+    exit()
+
+server_binding = ("localhost", 9999)
+cs.connect(server)
+
+data_from_server = cs.recv(1024)
+message = data_from_server.decode()
+print("[C]; the message was" + message)
+cs.send(input("Response here: ").encode())
+print("Done.")
+
+cs.close()
+exit()
