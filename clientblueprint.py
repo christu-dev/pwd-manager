f09eb961d95574c9589ed8f74cf066c83c17804d
Puyang He <128939003+Starscreen2@users.noreply.github.com>
2024-04-02 12:57:12 -0400

    Update clientblueprint.py

--- clientblueprint.py
+++ clientblueprint.py
@@ -39,61 +39,9 @@
 
 # # print("[c]: the message was: " + message)
 
-
-
-
 # # print("Done")
 # # #cs.close()
 # # exit()
-
-# import threading
-# import socket
-
-# try:
-#     cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     print("[c]: SOCKET CREATED")
-# except socket.error as err:
-#     print('socket open error: {} \n'.format(err))
-#     exit()
-
-# server_binding = ("localhost", 10000)
-# cs.connect(server_binding)
-
-# # username
-# data_from_server = cs.recv(1024)
-# message = data_from_server.decode()
-# print("[c]: the message was: " + message+"\n")
-# cs.send(input("Response here: ").encode())
-
-# # handle sign-up prompt
-# data_from_server = cs.recv(1024)
-# message = data_from_server.decode()
-# print("[c]: the message was: " + message+"\n")
-
-# if "not exist as an account" in message:
-#     response = input("Response here: ").encode()
-#     cs.send(response)
-#     if response.decode().lower() == 'y':
-#         # handle new password prompt
-#         data_from_server = cs.recv(1024)
-#         message = data_from_server.decode()
-#         print("[c]: the message was: " + message+"\n")
-#         cs.send(input("Response here: ").encode())
-
-# # password
-# data_from_server = cs.recv(1024)
-# message = data_from_server.decode()
-# print("[c]: the message was: " + message+"\n")
-# cs.send(input("Response here: ").encode())
-
-# # password check if correct
-# data_from_server = cs.recv(1024)
-# message = data_from_server.decode()
-# print("[c]: the message was: " + message)
-
-# print("Done")
-# cs.close()
-# exit()
 
 import threading
 import socket
